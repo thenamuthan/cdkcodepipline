c772a4cae37311338dbd11c93a26228a3fbebaf6
thenamuthan <thenamuthanme@gmail.com>
2024-07-30 17:06:42 +0530

    pushing code

--- bin/cdkcodepipline.ts
+++ bin/cdkcodepipline.ts
@@ -15,7 +15,7 @@ new CdkcodepiplineStack(app, 'CdkcodepiplineStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-   env: { account: '905418016855', region: 'us-east-1' },
+   env: { account: '905418016855', region: 'us-east-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
